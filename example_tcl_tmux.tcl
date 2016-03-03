4b90f305bcc90a7e8a0a3329f2d75384552d0dab
Yury Ershov <yuriy.ershov@gmail.com>
2016-03-03 13:16:16 +1100

    Added emacs-copy mode binding to config example

--- example_tcl_tmux.tcl
+++ example_tcl_tmux.tcl
@@ -7,5 +7,13 @@ bind-key -t vi-copy M-Enter tcl {send-keys "B E"}
 #bind-key -t vi-copy M-Enter tcl {display [copy-mode-selection]}
 
 # Open selection in a vim mini-window (no shell and files)
-bind-key -t vi-copy Y tcl {split-window -c [f #{pane_current_path}] -l 5 "echo -n [shell-quote [copy-mode-selection]] | vim -R -"}
+bind-key -t vi-copy y tcl {
+	split-window -c [f #{pane_current_path}] -l 5 "
+		echo -n [shell-quote [copy-mode-selection]] | vim -R -"
+}
+
+bind-key -t emacs-copy y tcl {
+	split-window -c [f #{pane_current_path}] -l 5 "
+		emacs --insert <(echo -n [shell-quote [copy-mode-selection]])"
+}
 
